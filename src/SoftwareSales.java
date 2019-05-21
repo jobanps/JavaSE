9cc6f1cf7e7751663005164560ad0446f6add3c2
748529 <748529@123-PC42.student.Tor.Loc>
2019-05-21 14:52:16 -0400

    softwareSales

---
+++ src/SoftwareSales.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class SoftwareSales {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter number of software packages required");
+		int softwareCount = input.nextInt();
+		
+		double discount, subTotal, discountAmount, finalAmount;
+		
+		subTotal = 99 * softwareCount;
+		
+		if(softwareCount >= 100 )
+			discount = 0.50;
+		else if(softwareCount >=50)
+			discount = 0.40;
+		else if(softwareCount >=20) 
+			discount = 0.30;
+		else if(softwareCount >=10)
+			discount = 0.10;
+		else 
+			discount = 0;
+	
+		discountAmount = subTotal * discount;
+		finalAmount = subTotal - discountAmount;
+		
+		System.out.println("Sub Total : $" + subTotal);
+		System.out.println("Final Total : $" + finalAmount);
+		System.out.println("Discount : " + (discount * 100) + "%");
+		System.out.println("Discount Amount : $" + discountAmount);
+		
+		input.close();
+		
+	}
+
+}
