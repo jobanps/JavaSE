8aa9ca4841f68423c6a5861b711519fae362b73b
Joban <Joban@LAPTOP-6GDEOP3F>
2019-06-04 14:16:15 -0400

    First function

--- src/week05/Functions.java
+++ src/week05/Functions.java
@@ -5,6 +5,16 @@ public class Functions {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		calcTax(100);
+	}
+	
+	/* Function to calculate the amount of Tax you need to pay */
+	
+	public static void calcTax(double price) {
+		
+		double taxAmount = price * 0.13;
+		System.out.println("Tax : " + taxAmount);
+		
 	}
 
 }
