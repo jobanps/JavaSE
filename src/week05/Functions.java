c591afb1bd6099889bcac6fa09aeb27e2db2f651
Joban <Joban@LAPTOP-6GDEOP3F>
2019-06-11 15:17:49 -0400

    Week06 To Do Print array done

--- src/week05/Functions.java
+++ src/week05/Functions.java
@@ -1,11 +1,16 @@
 package week05;
 
+import java.util.Scanner;
+
 public class Functions {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		calcTax(100);
+		//calcTax(100);
+		
+		System.out.println("Grade : " + testScores());
+		
 	}
 	
 	/* Function to calculate the amount of Tax you need to pay */
@@ -17,4 +22,50 @@ public class Functions {
 		
 	}
 
+	// 2as = v
+	
+	// P2:  Write a function that calculates 
+		// the distance that an object falls down (under gravity)
+		// d = 0.5*g*t    // g = gravity (9.8m/s^2)
+		// t = time 	  - how long its falling for
+		
+		// Your function 2 parameters:
+		// -- parameter 1  is gravity
+		// -- parameter 2 is time (in seconds)
+		// Your function should OUTPUT the result to console
+	
+	public static void calcDistance(double gravity, int time) {
+		
+		double distance = 0.5 * gravity * time;
+		
+		System.out.println("Distance : " + distance);
+	}
+	
+	public static String testScores() {
+		
+		Scanner input = new Scanner(System.in);
+		int marks;
+		int totalMarks = 0;
+		
+		for(int i = 1; i <= 5 ; i++) {
+			
+			System.out.println("Enter Test Score of Subject " + i);
+			marks = input.nextInt();
+			totalMarks += marks;
+		}
+		
+		double average = totalMarks/5.0;
+		
+		if(average >= 90)
+			return "A";
+		else if(average >= 80)
+			return "B";
+		else if(average >= 70)
+			return "C";
+		else if(average >= 60)
+			return "D";
+		else 
+			return "F";
+		
+	}
 }
---
+++ src/week06/ArrayListExamples.java
@@ -0,0 +1,35 @@
+package week06;
+
+public class ArrayListExamples {
+	
+	public static void main(String[] args) {
+		
+		// 1. Create a new List
+		List<String> names = new List<String>();
+		
+		// 1. Add 2 people to the list
+		
+		
+		// 2. Output total number of people in list
+		
+		
+		// 3. Delete someone from the list
+		
+		// 4. Output all people in the list
+		
+		
+		// 5. Get one person out of the list
+		
+		// 6. Change the name of the person
+		
+		// 7. Delete everyone from list
+		
+		// 8. Loop through every item in the list and output:
+		// HELLO _______ (where ___ is the name)
+		
+		
+	}
+	
+	
+
+}
