b934f0e7389356cb58430a52dd3aeeb4d39b8633
Joban <Joban@LAPTOP-6GDEOP3F>
2019-06-11 16:11:27 -0400

    ArrayList

--- src/week06/ArrayListExamples.java
+++ src/week06/ArrayListExamples.java
@@ -1,32 +1,56 @@
 package week06;
 
+
+import java.util.ArrayList;
+
+
 public class ArrayListExamples {
 	
 	public static void main(String[] args) {
-		
+
 		// 1. Create a new List
-		List<String> names = new List<String>();
+		ArrayList<String> names = new ArrayList<String>();
 		
 		// 1. Add 2 people to the list
-		
-		
+		names.add("Joban");
+		names.add("Gundeep");
+				
 		// 2. Output total number of people in list
 		
+		System.out.println("Number of Items in List : " + names.size());
 		
 		// 3. Delete someone from the list
 		
+		names.remove(1);
+		
+		
 		// 4. Output all people in the list
+		for(int i=0; i < names.size(); i++) {
+			
+			System.out.println(names.get(i));
 		
-		
+		}
 		// 5. Get one person out of the list
 		
-		// 6. Change the name of the person
 		
+		
+		// 6. Change the name of the person
+		names.set(0,"Jobanpreet");
+		
+		
+for(int i=0; i < names.size(); i++) {
+			
+			System.out.println(names.get(i));
+		
+		}
 		// 7. Delete everyone from list
+		
+		names.clear();
 		
 		// 8. Loop through every item in the list and output:
 		// HELLO _______ (where ___ is the name)
 		
+		System.out.println("size " + names.size());
 		
 	}
 	
--- src/week06/ArraysExamples.java
+++ src/week06/ArraysExamples.java
@@ -29,22 +29,72 @@ public class ArraysExamples {
 		// 5. Output everything in array
 		System.out.println("Everything in array: ");
 		// @TODO: Write code to output everything
-				
+		for(int i=0 ; i < names.length ; i++)
+			System.out.println("Item at " + i + " : " + names[i]);
+		
+		try {
 		// 6. Add something to the end of the array
-		names[7] = "Emad";
+		names[7] = "Emad"; 
+		System.out.println("Emad added to position 7");
+		} catch(Exception e) {
+			System.out.println("Error in adding Emad at 7th Posn");
+		}
 		// @TODO:  Write code to output names[7] to screen
+		
 		
 		
 		// 7. Remove someone from the array
 		// @TODO: Fill in code here
-		
-		
+		System.out.println("Removing item from the array");
+		names[3] = null;	// Removing item
 		// 8. Loop through every item in the array and output:
 		// HELLO _______ (where ___ is the name)
+		names[1] = "Joban";
+		names[2] = "Gundeep";
+		names[3] = "Harman";
 		
+		for(int i=0 ; i<names.length; i++) {
+			System.out.println(" HELLO " + names[i]);
+		
+		}
+		
+		for(int i=0 ; i<names.length; i++) {
+			System.out.print(" " + names[i]);
+		
+		}
 		
 		// 9. Shift everyone by 1 position
 		// @TODO: Fill in code here
+		/*for(int i=names.length-1 ; i>=1 ; i--) {
+			
+				names[i] = names[i-1];
+			
+		}
+		names[0] = null;
+		*/
+		
+		String temp = names[0];
+		String temp1;
+		for(int i = 1; i < names.length; i++) {
+			
+			temp1 = names[i];
+			names[i] = temp;
+			temp = temp1;
+		}
+		names[0] = null;
+		
+		/*String temp = names[0];
+		for(int i=0; i<names.length-1 ; i++) {
+			
+			names[i] = names[i+1];
+		}
+		names[names.length-1] = temp;
+		*/
+		System.out.println();
+		for(int i=0 ; i<names.length; i++) {
+			System.out.print(" " + names[i]);
+		
+		}
 	}
 
 }
